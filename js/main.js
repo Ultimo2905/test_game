c8552d747e10c3f99e0c60b412ba9b45d8eef561
Vladimir Kotorazhuk <43437388+Ultimo2905@users.noreply.github.com>
2019-10-29 15:03:37 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,9 @@
-// document.addEventListener('DOMContentLoaded', function() {
-//     document.querySelector('.userHeScore').insertAdjacentHTML("afterbegin", renderWinners(winners));
-// });
-
 function renderWinner(winner) {
     return `
 
-	<div class="userHeScor" style="background-color:grey">
-		<p>${winners.user}</p>
-		<p>${winners.date}</p>
+	<div class="userHeScorRow" style="background-color:grey">
+		<p>${winner.winner}</p>
+		<p>${winner.date}</p>
 	</div>
 `;
 }
@@ -16,12 +12,13 @@ function renderWinners(winners) {
     return winners.map(winner => renderWinner(winner))
         .join('');}
 
-fetch('http://starnavi-frontend-test-task.herokuapp.com/winners')
+fetch('http://starnavi-frontend-test-task.herokuapp.com/winners?per_page=12')
     .then(
         function(response) {
             response.json().then(function(winners) {
             	console.log(winners)
-                    document.querySelector('.userHeScore').insertAdjacentHTML('afterbegin', renderWinners(winners.winners));
+                    document.querySelector('.userHeScore').insertAdjacentHTML('afterbegin', renderWinners(winners));
             });
         }
     );
+
